ff2fbd006863391d1e847463311b44abdfc6f384
hex ( i use arch btw ) <karanlalwani.me@gmail.com>
2024-08-25 19:46:07 +0530

    Update EyeMotion.py

--- EyeMotion.py
+++ EyeMotion.py
@@ -1,9 +1,12 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import numpy as np
+
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size()
+
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
@@ -11,6 +14,7 @@ while True:
     output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
     frame_h, frame_w, _ = frame.shape
+
     if landmark_points:
         landmarks = landmark_points[0].landmark
         for id, landmark in enumerate(landmarks[474:478]):
@@ -21,13 +25,21 @@ while True:
                 screen_x = screen_w * landmark.x
                 screen_y = screen_h * landmark.y
                 pyautogui.moveTo(screen_x, screen_y)
+
         left = [landmarks[145], landmarks[159]]
+        left_distance = np.linalg.norm(np.array([left[0].x, left[0].y]) - np.array([left[1].x, left[1].y]))
+
         for landmark in left:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
-        if (left[0].y - left[1].y) < 0.004:
+
+        # Click detection threshold (adjust to your liking)
+        threshold = 0.01
+
+        if left_distance < threshold:
             pyautogui.click()
             pyautogui.sleep(1)
+
     cv2.imshow('Eye Controlled Mouse', frame)
     cv2.waitKey(1)
